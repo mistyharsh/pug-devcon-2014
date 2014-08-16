635ec055e64c04dfaf75e8d9b53a06c5ea8c2a28
Harshal <harshal.rp@gmail.com>
2014-08-16 15:04:52 +0530

    Completed the website for Webkit.
    Need to add support for firefox and IE

--- scripts/master.js
+++ scripts/master.js
@@ -1,86 +1,247 @@
 /* global jQuery: true,
     document: true,
-    window: true
+    window: true,
+    setTimeout: true,
+    setInterval: true
 */
+(function () {
 
-jQuery(document).ready(function ($) {
-    "use strict";
+    var ModalDialog,
+        prefix,
+        contentPanel,
+        cortanaLoop,
+        helpIcon;
 
-    setTimeout(function () {
+    jQuery(document).ready(function ($) {
+        "use strict";
 
-        $("#cortana-loader div")
-            .removeClass("fast-loop")
-            .addClass("slow-loop");
+        // Pause initial loader and start grow shrink behavior
+//        setTimeout(function () {
+//
+//            $("#cortana-loader div")
+//                .removeClass("fast-loop")
+//                .addClass("slow-loop");
+//
+//            //ModalDialog.show();
+//
+//        }, 5000);
 
-        //ModalDialog.show();
-
-    }, 4000);
+        cortanaLoop.start();
 
 
-    $("#cortana-command-input").keyup(function(event){
-        if(event.keyCode == 13){
-            ModalDialog.show();
-        }
-    });
+        // Handle cortana keypress event
+        $("#cortana-command-input").keyup(function(event) {
+            var inputText;
 
-});
-
-var ModalDialog = (function ($) {
-    "use strict";
-
-    var skeleton = "<div class='modal-dialog-fade'><div class='modal-dialog'></div></div>/",
-        fade = $(skeleton),
-        dialog = fade.find(".modal-dialog");
-
-    fade.on("click", function () {
-        ModalDialog.hide();
-    });
-
-    dialog.on({
-        webkitTransitionEnd: function () {
-            if(!dialog.hasClass("active")) {
-                fade.hide();
+            if (event.keyCode == 13) {
+                inputText = $(this).val().trim();
+                contentPanel.show(inputText);
             }
-        },
-        click: function () {
-            return false;
-        }
+        });
+
+        $(".help-icon").on("click", function () {
+            ModalDialog.show(helpIcon);
+        });
+
     });
 
-    $(function() {
-        $(document.body).append(fade);
-    });
 
-    return {
+    ModalDialog = (function ($) {
+        "use strict";
 
-        show: function (body) {
-            dialog.html(body || "");
-            fade.show();
+        var fade = null,
+            dialog = null,
+            body = null,
+            isOpen = false,
+            closeButton = null;
 
+        $(function() {
+            fade = $(".modal-dialog-fade");
+            dialog = fade.find(".modal-dialog");
+            body = dialog.find(".body");
+            closeButton = dialog.find(".close-button");
+            isOpen = false;
+
+            fade.on("click", function () {
+                if(isOpen) {
+                    ModalDialog.hide();
+                }
+            });
+
+            dialog.on({
+                webkitTransitionEnd: function () {
+                    if(isOpen === false) {
+                        fade.hide();
+                    }
+                },
+                click: function () {
+                    return false;
+                }
+            });
+
+            closeButton.on("click", function() {
+                if(isOpen) {
+                    ModalDialog.hide();
+                }
+            });
+
+        });
+
+        return {
+
+            show: function (content) {
+                isOpen = true;
+                if(!!content) {
+                    body.html(content);
+                }
+                fade.show();
+
+                setTimeout(function () {
+                    dialog.addClass("active");
+                }, 120);
+            },
+
+            hide: function () {
+                isOpen = false;
+                dialog.removeClass("active");
+            }
+
+        };
+
+
+    })(jQuery);
+
+    prefix = (function () {
+        var styles = window.getComputedStyle(document.documentElement, ''),
+            pre = (Array.prototype.slice.call(styles).join('').match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o']))[1],
+            dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
+
+        return {
+            dom: dom,
+            lowercase: pre,
+            css: '-' + pre + '-',
+            js: pre[0].toUpperCase() + pre.substr(1)
+        };
+
+    })();
+
+    helpIcon = (function ($) {
+        var skeleton = "<div class='help-message'>" +
+                            "<h3>Cortana accepts following commands.</h3>" +
+                            "<ol>" +
+                                "<li><a href='#' class='panel-link blue'>sessions</a></li>" +
+                                "<li><a href='#' class='panel-link blue'>sponsors</a></li>" +
+                                "<li><a href='#' class='panel-link blue'>speakers</a></li>" +
+                                "<li><a href='#' class='panel-link blue'>venue</a></li>" +
+                            "</ol>" +
+                            "<p>Just type and hit enter</p>" +
+                        "</div>";
+
+        $(function () {
+
+            $(".modal-dialog").on("click", ".panel-link", function() {
+                var anchor = $(this);
+
+                $("#cortana-command-input").val(anchor.text());
+                ModalDialog.hide();
+                contentPanel.show(anchor.text());
+
+                return false;
+            });
+
+        });
+
+
+        return skeleton;
+
+    })(jQuery);
+
+    contentPanel = (function ($) {
+
+        var panel,
+            items,
+            sessions,
+            sponsors,
+            speakers,
+            venue;
+
+        $(function () {
+            panel = $(".content-panel");
+            items = panel.find(".panel-item");
+            sessions = panel.find(".session");
+            sponsors = panel.find(".sponsors");
+            speakers = panel.find(".speakers");
+
+            items.on("webkitTransitionEnd", function () {
+                if (!$(this).hasClass("active")) {
+                    $(this).hide();
+                }
+            });
+        });
+
+        function show(panelObject) {
+            hide();
+            panelObject.show();
             setTimeout(function () {
-                dialog.addClass("active");
+                panelObject.addClass("active");
             }, 120);
-        },
-
-        hide: function () {
-            dialog.removeClass("active");
         }
 
-    };
+        function hide() {
+            panel.find(".active").removeClass("active");
+        }
 
+        return {
+            showSessions: function () {
+                show(sessions);
+            },
+            showSponsors: function () {
+                show(sponsors);
+            },
+            showSpeakers: function () {
+                show(speakers);
+            },
+            show: function (text) {
+                switch(text) {
+                    case "sessions":
+                        this.showSessions();
+                        break;
+                    case "sponsors":
+                        this.showSponsors();
+                        break;
+                    case "speakers":
+                        this.showSpeakers();
+                        break;
+                    case "venue":
+                        break;
+                }
+            },
+            hide: hide
+        };
 
-})(jQuery);
+    })(jQuery);
 
-var prefix = (function () {
-    var styles = window.getComputedStyle(document.documentElement, ''),
-        pre = (Array.prototype.slice.call(styles).join('').match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o']))[1],
-        dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
+    cortanaLoop = (function ($) {
+        var options = ["fast-loop", "slow-loop", "medium-loop"],
+            runOnce = false,
+            index = 0;
 
-    return {
-        dom: dom,
-        lowercase: pre,
-        css: '-' + pre + '-',
-        js: pre[0].toUpperCase() + pre.substr(1)
-    };
+        function loopThrough () {
+
+            if(!runOnce) {
+                runOnce = true;
+                setInterval(function () {
+                    $("#cortana-loader div")
+                        .removeClass(options[index])
+                        .addClass(options[index = (index + 1) % 3]);
+                }, 8000);
+            }
+        }
+
+        return {
+            start: loopThrough
+        };
+
+    })(jQuery);
 
 })();
